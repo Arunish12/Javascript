8312b4b03470728837f5f7c6991057258cb9b7c5
Arunish12 <arunishkumar11@gmail.com>
2023-10-20 15:28:40 +0530

    function

--- Syntax/session1.js
+++ Syntax/session1.js
@@ -9,6 +9,20 @@
 // let isFalse = !isTrue; // Logical NOT
 
 
+// Palindrome Checker
+
+// function PalindromeChecker (a){
+//   var str = String(a);
+//   var reversedStr = str.split('').reverse().join('');
+//   if (str === reversedStr) {
+//     return true;
+//     } else {
+//       return false;
+//       }
+// }
+
+
+
 
 // This is a single-line comment
 
@@ -20,10 +34,75 @@
 
 // Functions
 
-function sayHello(name) {
-  console.log("Hello, " + name + "!");
-}
+// function sayHello(name) {
+//   console.log("Hello, " + name + "!");
+// }
 
-const greet = (name) => {
-  console.log(`Hello, ${name}!`);
-}
+// const greet = (name) => {
+//   console.log(`Hello, ${name}!`);
+// }
+
+// sayHello('Arunish')
+
+// //function declaration
+// function run(){
+//   console.log("running");
+// }
+// run();
+
+
+// // Named function assignment 
+// let stand = function walk() {
+//     console.log('walking');
+// };
+
+// //arrow function
+// const square = (x) => x * x;
+// console.log(square(5));
+
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const sum = add(2,4);
+// console.log(sum); 
+
+
+// function rootoffun (a, b, c){
+//   let root_D = Math.sqrt(b*b - 4*a*c)
+//   let x = (-b + root_D)/2*a
+//   let y = (-b - root_D)/ 2*a
+//   console.log('first root is ', x, '\n second root is ', y)
+
+//   alert(`first root is ${x}, second root is ${y}`);
+// }
+
+// rootoffun(1,-3,2)
+
+
+// Palindrome Checker
+
+// function PalindromeChecker (a){
+  
+// let txt = a.toString();
+// var reversedTxt = txt.split('').reverse().join('');
+// if (txt === reversedTxt) {
+//   console.log(`${txt} is palindrome`);} else{
+//     console.log(`${txt} is not palindrome`);}
+
+// }
+
+// PalindromeChecker ('radar')
+// PalindromeChecker ('MADAM')
+// PalindromeChecker ('Rajnish')
+
+//Split and Join
+
+// var sudoname = 'Rajnish';
+// listName = sudoname.split('')
+// console.log(listName);
+
+// JoinName = listName.join('')
+// console.log(JoinName);
+
